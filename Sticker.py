1d825df3830cb50d79c56652a2cb30f6accbb54c
JeongHeon Woo <contact@wookingwoo.com>
2021-02-26 02:20:36 +0900

    Add zeroing_y

--- Sticker.py
+++ Sticker.py
@@ -25,7 +25,7 @@ class Sticker(QtWidgets.QMainWindow):
 
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.__walkHandler)
-        self.timer.start(1000 / self.speed)
+        self.timer.start(int(1000 / self.speed))
 
     def __walkHandler(self):
         if self.xy[0] >= self.to_xy[0]:
@@ -54,10 +54,10 @@ class Sticker(QtWidgets.QMainWindow):
         centralWidget = QtWidgets.QWidget(self)
         self.setCentralWidget(centralWidget)
 
-        flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint if self.on_top else QtCore.Qt.FramelessWindowHint)
+        flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint if self.on_top else QtCore.Qt.FramelessWindowHint) # 창틀과 타이틀바 숨기기
         self.setWindowFlags(flags)
-        self.setAttribute(QtCore.Qt.WA_NoSystemBackground, True)
-        self.setAttribute(QtCore.Qt.WA_TranslucentBackground, True)
+        self.setAttribute(QtCore.Qt.WA_NoSystemBackground, True) # 배경 제거
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground, True) # 배경 투명
 
         label = QtWidgets.QLabel(centralWidget)
         movie = QMovie(self.img_path)
@@ -65,37 +65,41 @@ class Sticker(QtWidgets.QMainWindow):
         movie.start()
         movie.stop()
 
-        w = int(movie.frameRect().size().width() * self.size)
-        h = int(movie.frameRect().size().height() * self.size)
+        w = int(movie.frameRect().size().width() * self.size) #너비
+        h = int(movie.frameRect().size().height() * self.size) #높이
         movie.setScaledSize(QtCore.QSize(w, h))
         movie.start()
 
         self.setGeometry(self.xy[0], self.xy[1], w, h)
 
-    def mouseDoubleClickEvent(self, e):
+    def mouseDoubleClickEvent(self, e): # 더블클릭하면 종료
         QtWidgets.qApp.quit()
 
 if __name__ == '__main__':
+
+
     app = QtWidgets.QApplication(sys.argv)
 
-    s = Sticker('gif/left.gif', xy=[-80, 200], on_top=False)
+    zeroing_y = 230
 
-    s1 = Sticker('gif/amongus/red_vent.gif', xy=[780, 1020], size=0.3, on_top=True)
+    s = Sticker('gif/left.gif', xy=[-80, zeroing_y+200], on_top=False) # on_top=False: 항상 위 해제
 
-    s2 = Sticker('gif/amongus/orange.gif', xy=[1200, 1020], size=0.3, on_top=True)
-    s2.walk(from_xy=[1200, 1010], to_xy=[1220, 1020])
+    s1 = Sticker('gif/amongus/red_vent.gif', xy=[780, zeroing_y+1020], size=0.3, on_top=True)
 
-    s3 = Sticker('gif/amongus/blue_green.gif', xy=[400, 920], size=1.0, on_top=True)
+    s2 = Sticker('gif/amongus/orange.gif', xy=[1200, zeroing_y+1020], size=0.3, on_top=True)
+    s2.walk(from_xy=[1200, zeroing_y+1010], to_xy=[1220, zeroing_y+1023]) # 움직이기
 
-    s4 = Sticker('gif/amongus/mint.gif', xy=[1000, 950], size=0.2, on_top=True)
-    s4.walk(from_xy=[900, 950], to_xy=[1100, 950], speed=120)
+    s3 = Sticker('gif/amongus/blue_green.gif', xy=[400, zeroing_y+920], size=1.0, on_top=True)
 
-    s5 = Sticker('gif/amongus/brown.gif', xy=[200, 1010], size=0.75, on_top=True)
+    s4 = Sticker('gif/amongus/mint.gif', xy=[1000, zeroing_y+950], size=0.2, on_top=True)
+    s4.walk(from_xy=[900, zeroing_y+950], to_xy=[1100, zeroing_y+950], speed=100) # 움직이기
 
-    s6 = Sticker('gif/amongus/yellow.gif', xy=[1850, 800], size=0.75, on_top=True)
-    # s6.walk(from_xy=[0, 800], to_xy=[1850, 800], speed=240)
+    s5 = Sticker('gif/amongus/brown.gif', xy=[200, zeroing_y+1010], size=0.75, on_top=True)
 
-    s7 = Sticker('gif/amongus/magenta.gif', xy=[1500, 900], size=0.5, on_top=True)
-    s7.walk(from_xy=[1400, 900], to_xy=[1600, 900], speed=180)
+    s6 = Sticker('gif/amongus/yellow.gif', xy=[2000, zeroing_y+800], size=0.75, on_top=True)
+    s6.walk(from_xy=[1900, zeroing_y+ 810], to_xy=[2000, zeroing_y+800], speed=140)
+
+    s7 = Sticker('gif/amongus/magenta.gif', xy=[1550, zeroing_y+900], size=0.5, on_top=True)
+    s7.walk(from_xy=[1500, zeroing_y+900], to_xy=[1600, zeroing_y+900], speed=180)
 
     sys.exit(app.exec_())
